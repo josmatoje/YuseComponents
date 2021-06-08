4ff123bf4b0c957c374f179beb074763aecc054d
FinalMatoje <josemariamataojeda@gmail.com>
2021-06-08 13:45:43 +0200

    fin bbdd

--- out/production/YuseComponents/Script YuseComponents.sql
+++ out/production/YuseComponents/Script YuseComponents.sql
@@ -1,9 +1,11 @@
 USE master
 GO
-IF NOT EXISTS (SELECT * FROM master.dbo.SYSDATABASES WHERE NAME = 'YuseComponents') --Si no existe la base de datos
-	CREATE DATABASE YuseComponents
-ELSE
+IF EXISTS (SELECT * FROM master.dbo.SYSDATABASES WHERE NAME = 'YuseComponents') --Si no existe la base de datos
+BEGIN
 	DROP DATABASE YuseComponents
+END
+
+CREATE DATABASE YuseComponents
 GO
 
 USE YuseComponents
@@ -12,66 +14,82 @@ GO
 CREATE TABLE Usuarios(
 	IDUsuario uniqueidentifier CONSTRAINT PK_Usuarios Primary Key,
 	NickUsuario varchar(30) NOT NULL,
-	Nombre varchar(30) NOT NULL,
+	Nombre varchar(30) NULL,
 	Apellido varchar(50) NULL,
-	Saldo money NOT NULL Default 0,
-	Descuento decimal (3,2) NULL CONSTRAINT CK_Descuento Check (Descuento BETWEEN -1 AND 1)
+	Saldo money NULL Default 0,
+	Descuento decimal (3,2) NOT NULL DEFAULT 0,
+	CONSTRAINT CK_Descuento Check (Descuento BETWEEN 0 AND 1)
 )
 GO
 
 CREATE TABLE ListasCreadas(
-	[Nº de lista] int IDENTITY(10000,1) CONSTRAINT PK_Usuarios Primary Key,
+	[Nº de lista] int IDENTITY(10000,1) CONSTRAINT PK_Lista Primary Key,
 	Nombre varchar(20) NOT NULL,
 	IDUsuario uniqueidentifier CONSTRAINT FK_CreaListaUsuaria Foreign Key REFERENCES Usuarios (IDUsuario),
-	Publica char(2) NOT NULL Default 'SI',--Igual BIT es mejor
-	CONSTRAINT CK_PublicaSi Check (Publica LIKE 'SI'),
-	CONSTRAINT CK_PublicaNo Check (Publica LIKE 'NO'),
-	Privada AS CASE WHEN (Publica='SI') THEN 'NO' WHEN (Publica='NO') THEN 'SI' END
+	Publica bit NOT NULL Default 1,
+	Privada AS CASE WHEN (Publica=1) THEN 0 ELSE 1 END
 )
 GO
 
 CREATE TABLE Transacciones(
 	IDTransaccion uniqueidentifier CONSTRAINT PK_Transaccion Primary Key,
 	IDUsuario uniqueidentifier CONSTRAINT FK_TransaccionUsuario Foreign Key REFERENCES Usuarios (IDUsuario),
+	[Nº de lista] int NOT NULL CONSTRAINT FK_ListaTransacciones Foreign Key REFERENCES ListasCreadas ([Nº de lista]),
 	[Fecha y hora] datetime2 NOT NULL Default CURRENT_TIMESTAMP,
 	[Total cobrado] money NULL Default NULL
 )
 GO
 
-CREATE TABLE TipoComponente(
-	Nombre varchar(20) CONSTRAINT PK_TipoComponente Primary Key
+CREATE TABLE TiposComponente(
+	[Nombre tipo] varchar(20) CONSTRAINT PK_TipoComponente Primary Key
+)
+GO
+
+CREATE TABLE Propiedades(
+	Propiedad varchar(20) CONSTRAINT PK_Propiedad Primary Key
+)
+GO
+
+CREATE TABLE Propiedades_Tipos(
+	Propiedad varchar(20) CONSTRAINT FK_PriopiedadTipo Foreign Key REFERENCES Propiedades (Propiedad),
+	Tipo varchar(20) CONSTRAINT FK_TipoPriopieda Foreign Key REFERENCES TiposComponente ([Nombre tipo]),
+	CONSTRAINT PK_PropiedadTipo Primary Key (Propiedad, Tipo)
 )
 GO
 
 CREATE TABLE Componentes(
-	ProductId uniqueidentifier CONSTRAINT PK_Componentes Primary Key,
-	TipoComponente varchar(20) CONSTRAINT FK_TipoComponente Foreign Key REFERENCES TipoComponente (Nombre),
+	IdComponente uniqueidentifier CONSTRAINT PK_Componentes Primary Key,
+	TipoComponente varchar(20) CONSTRAINT FK_TipoComponente Foreign Key REFERENCES TiposComponente ([Nombre tipo]),
 	Nombre varchar(50) NOT NULL,
 	Precio money NOT NULL,
 	Cantidad int NULL
 )
 GO
 
-CREATE TABLE Propiedades(
-	Propiedad varchar(20) CONSTRAINT PK_TipoComponente Primary Key,
-	Valor varchar(30) NULL
+CREATE TABLE Propiedades_Componentes(
+	IdComponente uniqueidentifier CONSTRAINT FK_ComponentesPropiedades Foreign Key REFERENCES Componentes (IdComponente),
+	Propiedad varchar(20) CONSTRAINT FK_PropiedadesComponentes Foreign Key REFERENCES Propiedades (Propiedad),
+	Valor varchar(15) NULL,
+	Unidad varchar(10) NULL,
+	CONSTRAINT PK_PropiedadComponente Primary Key (IdComponente, Propiedad)
 )
 GO
 
-CREATE TABLE TransaccionListaComponente(
-	[Nº de lista] int IDENTITY(10000,1) CONSTRAINT PK_Usuarios Primary Key,
-	Nombre varchar(20) NOT NULL,
-	Creador uniqueidentifier CONSTRAINT FK_CreaListaUsuaria Foreign Key REFERENCES Usuarios
-
+CREATE TABLE ListaComponente(
+	[Nº de lista] int NOT NULL CONSTRAINT FK_ListaComponente Foreign Key REFERENCES ListasCreadas ([Nº de lista]),
+	IdComponente uniqueidentifier CONSTRAINT FK_ComponentesLista Foreign Key REFERENCES Componentes (IdComponente),
+	CONSTRAINT PK_ListaComponente Primary Key ([Nº de lista], IdComponente)
 )
 GO
 
-
+IF NOT EXISTS (SELECT name FROM [sys].[server_principals] WHERE name = 'pedrito')
+BEGIN
 --Creacion del usuario de la base de datos
-CREATE LOGIN pedrito with password='megustan LAS bases de datos', 
-DEFAULT_DATABASE=YuseComponents 
+	CREATE LOGIN pedrito with password='megustan LAS bases de datos', 
+	DEFAULT_DATABASE=YuseComponents 
 
-CREATE USER pedrito FOR LOGIN pedrito 
-GRANT EXECUTE, SELECT --Solo se realizan procedimientos para todas las acciones que suceden en el programa
---ON ListasCreadas.YuseComponents --Si GRANT INSERT, UPDATE, DELETE
-TO pedrito
+	CREATE USER pedrito FOR LOGIN pedrito 
+	GRANT EXECUTE, SELECT --Solo se realizan procedimientos para todas las acciones que suceden en el programa
+	--ON ListasCreadas.YuseComponents --Si GRANT INSERT, UPDATE, DELETE
+	TO pedrito
+END
--- src/Principal/Main.java
+++ src/Principal/Main.java
@@ -1,15 +1,17 @@
 package Principal;
 
 
+import JavaFX.JavaFX;
+
 import static javafx.application.Application.launch;
 
-public class Main{
+public class Main extends Aplication{
     /**
      * The main method is only needed for the IDE with limited
      * JavaFX support. Not needed for running from the command line.
      */
     public static void main(String[] args) {
-        launch(args);
+        JavaFX.launch(args);
     }
 
 }
